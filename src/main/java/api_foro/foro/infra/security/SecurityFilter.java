b29d45507749383c204ef2e763c9b568629adfac
erika.duarte <erika.duarte.mora@gmail.com>
2024-07-16 09:46:11 -0500

    filtro de seguridad para el token

---
+++ src/main/java/api_foro/foro/infra/security/SecurityFilter.java
@@ -0,0 +1,30 @@
+package api_foro.foro.infra.security;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+       //Obtener token del header
+        var token = request.getHeader("Authorization");
+        if(token == null || token == ""){
+            throw new RuntimeException("El token enviado no es válido");
+        }
+        token = token.replace("Bearer ", "");
+        System.out.println(token);
+        filterChain.doFilter(request, response);
+
+    }
+}
